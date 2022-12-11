1eb2936cc2213a1cd3876d341906d9393093917b
sshravan2292 <sshravan2292@gmail.com>
2022-12-11 22:12:20 +0530

    ServiceImpl Commit

---
+++ src/main/java/in/ashokit/binding/Contact.java
@@ -0,0 +1,23 @@
+package in.ashokit.binding;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "CONTACT_DETAILS")
+public class Contact {
+	
+	@Id
+	@GeneratedValue
+	private Integer contactId;
+	private String contactName;
+	private String contactEmail;
+	private Long contactNum;
+	
+
+}
